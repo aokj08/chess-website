aa60ddeb7d09b229cab7775a8208dcfb8813ee85
aokj08 <kejian0806@gmail.com>
2026-08-10 14:57:57 -0700

    Link external CSS

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const path = require("path");
 // allows the handler to parse stuff 
 app.use(parser.urlencoded({extended: false}));
 
+// serve static files (CSS, images, client JS) from the public folder
+app.use(express.static(path.join(__dirname, "public")));
+
 // .use accepts middleware functions which handle http request
 app.use("/home", homeRoute);
 
